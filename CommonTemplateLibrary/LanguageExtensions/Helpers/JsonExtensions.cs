8ddb849521eb3da81f6a1ae11329ad037680234a
Karen Payne <paynekaren@comcast.net>
2023-07-31 02:47:21 -0700

    documentation

--- CommonTemplateLibrary/LanguageExtensions/Helpers/JsonExtensions.cs
+++ CommonTemplateLibrary/LanguageExtensions/Helpers/JsonExtensions.cs
@@ -6,12 +6,19 @@ using System.Text.Json;
 namespace CommonTemplateLibrary.LanguageExtensions.Helpers;
 public static class JsonExtensions
 {
+    /// <summary>
+    /// Deserialize file
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    /// <param name="sender"></param>
+    /// <param name="fileName">File name or variable for file name</param>
     [SourceTemplate]
     [Macro(Target = "T")]
+    [Macro(Target = "list")]
     [Macro(Target = "fileName")]
     public static void deserialize<T>(this T sender, string fileName)
     {
-        List<T> customersList =
+        List<T> list =
             JsonSerializer.Deserialize<List<T>>(
                 File.ReadAllText(fileName))!;
     }
