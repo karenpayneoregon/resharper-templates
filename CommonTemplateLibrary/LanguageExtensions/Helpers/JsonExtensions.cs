e32c2c9664409e811651e0a6debb2804be21de5f
Karen Payne <paynekaren@comcast.net>
2023-07-31 02:43:06 -0700

    new template, new test project

---
+++ CommonTemplateLibrary/LanguageExtensions/Helpers/JsonExtensions.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using System.Text.Json;
+
+// ReSharper disable InconsistentNaming
+
+namespace CommonTemplateLibrary.LanguageExtensions.Helpers;
+public static class JsonExtensions
+{
+    [SourceTemplate]
+    [Macro(Target = "T")]
+    [Macro(Target = "fileName")]
+    public static void deserialize<T>(this T sender, string fileName)
+    {
+        List<T> customersList =
+            JsonSerializer.Deserialize<List<T>>(
+                File.ReadAllText(fileName))!;
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using CommonTemplateLibrary.LanguageExtensions.Helpers;
+using ConsoleApp1.Models;
+
+namespace ConsoleApp2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        TestDeserializing();
+        Console.ReadLine();
+    }
+
+    /// <summary>
+    /// typeof(Customers).deserialize
+    /// </summary>
+    private static void TestDeserializing()
+    {
+        var fileName = "Customers.json";
+    }
+}
